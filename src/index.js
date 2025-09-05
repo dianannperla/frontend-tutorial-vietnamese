1130af3740d57bc78e910bf94e0b08318acd9595
dianannperla <waafirmelyza@hotmail.com>
2025-09-05 19:19:19 +0100

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Chào mừng đến với Frontend Tutorial!</h1>
+            <p>Khám phá các framework frontend như React, Vue.js và Angular.</p>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
